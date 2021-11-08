85bb923f1317af6db958e24e0f1f30f563f71bcc
Michatorski <MichalHomend@protonmail.com>
2021-11-08 23:05:13 +0100

    making more of the task of Mikel

---
+++ TaskInversionControl/src/main/java/org/iesfm/asociacion/service/MemberService.java
@@ -0,0 +1,37 @@
+package org.iesfm.asociacion.service;
+
+import org.iesfm.asociacion.Member;
+import org.iesfm.asociacion.dao.MemberDAO;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class MemberService implements MemberDAO {
+    private Map<String, Member> members = new HashMap<>();
+
+    private NamedParameterJdbcTemplate jdbc;
+
+    @Override
+    public List<Member> list() {
+        return new LinkedList<>(members.values());
+    }
+
+    @Override
+    public void insert(Member member) {
+        members.put(member.getNif(), member);
+
+    }
+
+    @Override
+    public void delete(String memberNif) {
+        members.remove(memberNif);
+    }
+
+    @Override
+    public void modify(String nif, Member updateMember) {
+        members.replace(nif, updateMember);
+    }
+}
