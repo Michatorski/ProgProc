6880b2df4a82befd4be5e97031ae9f27823e19fb
michote <56888952+Michatorski@users.noreply.github.com>
2021-11-10 14:20:24 +0100

    Comprobas IoC

---
+++ ShopREST/src/main/java/org/iesfm/shop/controller/ArticleController.java
@@ -0,0 +1,7 @@
+package org.iesfm.shop.controller;
+
+import org.iesfm.shop.dao.ArticleDAO;
+
+public class ArticleController {
+    private ArticleDAO articleDAO;
+}
---
+++ ShopREST/src/main/java/org/iesfm/shop/controller/ClientController.java
@@ -0,0 +1,7 @@
+package org.iesfm.shop.controller;
+
+import org.iesfm.shop.dao.ClientDAO;
+
+public class ClientController {
+    private ClientDAO clientDAO;
+}
